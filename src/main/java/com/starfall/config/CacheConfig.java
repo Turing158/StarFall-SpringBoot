dc1264508e450b02305bcfdf530fb590bec3da2d
Turing158 <15818961209@163.com>
2024-03-20 01:02:36 +0800

    init

---
+++ src/main/java/com/starfall/config/CacheConfig.java
@@ -0,0 +1,28 @@
+package com.starfall.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public KeyGenerator keyGenerator(){
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object i : params) {
+                    sb.append(i);
+                }
+                return null;
+            }
+        };
+    }
+}
