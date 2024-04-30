68e23eef059895defbaee88605d62910b1919d5c
Turing158 <15818961209@163.com>
2024-04-30 20:49:27 +0800

    添加前端页面访问是否有权限，无权限直接返回

--- src/main/java/com/starfall/controller/OtherController.java
+++ src/main/java/com/starfall/controller/OtherController.java
@@ -1,12 +1,12 @@
 package com.starfall.controller;
 
+import com.starfall.entity.ResultMsg;
 import com.starfall.service.OtherService;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
@@ -17,11 +17,17 @@ public class OtherController {
     @Autowired
     OtherService otherService;
 
-    @RequestMapping("/getCodeImage")
+    @GetMapping("/getCodeImage")
     public void getCodeImage(
             HttpSession session,
             HttpServletResponse resp
     ) throws IOException {
         otherService.getCodeImage(session,resp);
+    }
+
+
+    @PostMapping("/toAdmin")
+    public ResultMsg toAdmin(@RequestHeader("Authorization") String token){
+        return otherService.toAdmin(token);
     }
 }
--- src/main/java/com/starfall/service/OtherService.java
+++ src/main/java/com/starfall/service/OtherService.java
@@ -1,12 +1,18 @@
 package com.starfall.service;
 
 import cn.hutool.captcha.GifCaptcha;
+import com.starfall.dao.UserDao;
+import com.starfall.entity.ResultMsg;
+import com.starfall.entity.User;
+import com.starfall.util.JwtUtil;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -16,6 +22,8 @@ public class OtherService {
 
     @Autowired
     StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    UserDao userDao;
 
     public void getCodeImage(HttpSession session, HttpServletResponse resp) throws IOException {
         ServletOutputStream sos = resp.getOutputStream();
@@ -23,5 +31,17 @@ public class OtherService {
         g.createCode();
         session.setAttribute("code",g.getCode());
         g.write(sos);
+    }
+
+    public ResultMsg toAdmin( String token){
+
+        Claims claims = JwtUtil.parseJWT(token);
+        String role = (String) claims.get("ROLE");
+        String user = (String) claims.get("USER");
+        User userObj = userDao.findByUserOrEmail(user);
+        if(!(role.equals("admin") || userObj.getRole().equals("admin"))){
+            return ResultMsg.error("NOT_PERMISSION");
+        }
+        return ResultMsg.success();
     }
 }
